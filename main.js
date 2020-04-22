0146a7c3ef2ff0fa362b5306e182c3b64a4aa593
Luke Lancaster <luketlancaster@gmail.com>
2020-04-21 21:52:56 -0500

    Implement buildAssignmentCard
    And go a little nuts on the styling

--- main.js
+++ main.js
@@ -22,7 +22,6 @@ const dinos = [
 ];
 
 const printToDom = (selector, textToPrint) => {
-  console.log('in printToDom', selector, textToPrint);
   const selectedDiv = document.querySelector(selector);
   selectedDiv.innerHTML = textToPrint;
 }
@@ -40,8 +39,56 @@ const buildDinoCards = (dinoCollection) => {
   printToDom('#dino-pen', domString);
 }
 
+const assignments = [
+  {
+    title: 'product cards',
+    dueDate: '03/05/2019',
+    topic: 'HTML/CSS',
+    notes: 'Use flexbox',
+  },
+  {
+    title: 'product cards2',
+    dueDate: '03/05/2019',
+    topic: 'HTML/CSS',
+    notes: 'Use flexbox',
+  },
+  {
+    title: 'product cards3',
+    dueDate: '03/05/2019',
+    topic: 'HTML/CSS',
+    notes: 'Use flexbox',
+  },
+  {
+    title: 'product cards4',
+    dueDate: '03/05/2019',
+    topic: 'HTML/CSS',
+    notes: 'Use flexbox',
+  },
+  {
+    title: 'product cards5',
+    dueDate: '03/05/2019',
+    topic: 'HTML/CSS',
+    notes: 'Use flexbox',
+  }
+];
+
+const buildAssignmentCards = (assignmentCollection) => {
+  let domString = '';
+  for (let j = 0; j < assignmentCollection.length; j++) {
+    domString += `<div class="assignment">`
+    domString += `<div class="assignment-title">${assignmentCollection[j].title}</div>`;
+    domString += `<strong>Due Date: ${assignmentCollection[j].dueDate}</strong>`
+    domString += `<div>Topic: ${assignmentCollection[j].topic}</div>`
+    domString += `<div>Notes: ${assignmentCollection[j].notes}</div>`
+    domString += `</div>`
+  }
+
+  printToDom('#assignments', domString)
+};
+
 const init = () => {
   buildDinoCards(dinos);
+  buildAssignmentCards(assignments);
 }
 
 init();
